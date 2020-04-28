9da7827df24b0ac39482ddecada4bcfc8477237e
Kieran O'Day <Kieran@Kierans-MBP.fios-router.home>
2020-04-28 16:24:37 -0400

    cart icon and (hopefully) ios fix

--- src/Shop.js
+++ src/Shop.js
@@ -8,9 +8,43 @@ import Cart from './Cart';
 import Footer from './Footer';
 
 class Shop extends Component {
+    constructor() {
+        super();
+        this.state = {
+            width: window.innerWidth,
+        };
+    }
+
+    componentWillMount() {
+        window.addEventListener('resize', this.handleWindowSizeChange);
+    }
+    // make sure to remove the listener
+    // when the component is not mounted anymore
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleWindowSizeChange);
+    }
+
+    handleWindowSizeChange = () => {
+        this.setState({ width: window.innerWidth });
+    };
+    cartNum = () => {
+        var num = 0;
+        Object.keys(this.props.cart).forEach(key => {
+            num += this.props.cart[key]
+        })
+        return num;
+    }
     render() {
         return (
             <div>
+                {this.state.width < 768 ?
+                    <div className='cartIconBox' onClick={() => this.refs.cart.scrollIntoView({ behavior: 'smooth' })}>
+                        <div className='cartIconNum'>{this.cartNum()}</div>
+                        <i className='fa fa-shopping-cart centered'></i>
+                    </div>
+                    :
+                    null
+                }
                 <div class="parallax-wrapper">
                     <div className='parallax-padding'>
                         <h1 className='parallax-heading'>STUDIODAY <p className='carrot' onClick={() => this.refs.content.scrollIntoView({ behavior: 'smooth' })}>&#9660;</p></h1>
@@ -23,13 +57,13 @@ class Shop extends Component {
                                         {
                                             this.props.items ?
                                                 <>
-                                                    {Object.keys(this.props.items).sort((a,b) => {
+                                                    {Object.keys(this.props.items).sort((a, b) => {
                                                         return this.props.items[b]['stock'] - this.props.items[a]['stock']
                                                     }).map(key => {
                                                         const item = this.props.items[key]
                                                         return (
                                                             <div className='col-xl-4 col-md-6 col-12 noPadding'>
-                                                                <div className='shopItemContainer'>
+                                                                <div className='shopItemContainer' onClick=''>
                                                                     <img className='shopItemImg' src={item['image']} />
                                                                     <div className='shopItemInfo'>
                                                                         <div className='text'>
@@ -41,8 +75,8 @@ class Shop extends Component {
                                                                     {item['stock'] <= 0 ?
                                                                         <p className='outOfStock'>Out of Stock</p>
                                                                         :
-                                                                        <div className ='outOfStock text-right'>
-                                                                            <p style={{marginBottom:'0px'}}>{item['stock']}</p>
+                                                                        <div className='outOfStock text-right'>
+                                                                            <p style={{ marginBottom: '0px' }}>{item['stock']}</p>
                                                                             <p>Stock</p>
                                                                         </div>
                                                                     }
@@ -56,7 +90,7 @@ class Shop extends Component {
                                         }
                                     </div>
                                 </div>
-                                <div className='col-lg-3 col-md-4 col-12' style={{ backgroundColor: '#fff9f9' }}> 
+                                <div ref='cart' className='col-lg-3 col-md-4 col-12' style={{ backgroundColor: '#fff9f9' }}>
                                     <Cart checkoutLink={true}></Cart>
                                 </div>
                             </div>
